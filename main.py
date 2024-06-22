aafc5b28a5bc8af20bdaef0e4b8a619f4f8b0fd5
Andriel Abreu Rosa <diel132619@gmail.com>
2024-06-22 12:00:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+n1 =float(input("Digite o primeiro numero: "))
+n2 = float(input("Digite o segundo  numero: "))
+if n1 > n2 :
+  print("O primeiro numero é maior que o segundo")
+else:
+  print("O segundo numero é maior que o primeiro")
+  
+
